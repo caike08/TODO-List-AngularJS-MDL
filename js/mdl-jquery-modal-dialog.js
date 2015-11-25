56bb403593e7c045e88d7b9edfc5ed432bdc7e79
Caike Motta <caike-motta@hotmail.com>
2015-11-25 11:29:43 -0200

    First pages commit

---
+++ js/mdl-jquery-modal-dialog.js
@@ -0,0 +1,107 @@
+function showDialog(options) {
+    options = $.extend({
+        id: 'orrsDiag',
+        title: null,
+        input: null,
+        negative: false,
+        positive: false,
+        cancelable: true,
+        contentStyle: null,
+        onLoaded: false
+    }, options);
+
+    // remove existing dialogs
+    $('.dialog-container').remove();
+    $(document).unbind("keyup.dialog");
+
+    $('<div id="' + options.id + '" class="dialog-container"><div class="mdl-card mdl-shadow--16dp"></div></div>').appendTo("body");
+    
+    var dialog = $('#orrsDiag');
+    
+    var content = dialog.find('.mdl-card');
+    
+    if (options.contentStyle != null) content.css(options.contentStyle);
+    if (options.title != null) {
+        $('<h5>' + options.title + '</h5>').appendTo(content);
+    }
+    /*
+     * OLD!
+    if (options.input != null) {
+        
+    }*/
+
+    /* TODO: Insert a texfield into the frame */
+    /*
+
+    $('<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input" type="text" id="sample3">
+                <label class="mdl-textfield__label" for="sample3"> 
+                    What are you about to do?"
+                </label>
+        </div>').appendTo(content);
+
+    */
+
+    if (options.negative || options.positive) {
+        var buttonBar = $('<div class="mdl-card__actions dialog-button-bar"></div>');
+        if (options.negative) {
+            options.negative = $.extend({
+                id: 'negative',
+                title: 'Cancel',
+                onClick: function () {
+                    return false;
+                }
+            }, options.negative);
+            var negButton = $('<button class="mdl-button mdl-js-button mdl-js-ripple-effect" id="' + options.negative.id + '">' + options.negative.title + '</button>');
+            negButton.click(function (e) {
+                e.preventDefault();
+                if (!options.negative.onClick(e))
+                    hideDialog(dialog)
+            });
+            negButton.appendTo(buttonBar);
+        }
+        if (options.positive) {
+            options.positive = $.extend({
+                id: 'positive',
+                title: 'OK',
+                onClick: function () {
+                    return false;
+                }
+            }, options.positive);
+            var posButton = $('<button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" id="' + options.positive.id + '">' + options.positive.title + '</button>');
+            posButton.click(function (e) {
+                e.preventDefault();
+                if (!options.positive.onClick(e))
+                    hideDialog(dialog)
+            });
+            posButton.appendTo(buttonBar);
+        }
+        buttonBar.appendTo(content);
+    }
+    componentHandler.upgradeDom();
+    if (options.cancelable) {
+        dialog.click(function () {
+            hideDialog(dialog);
+        });
+        $(document).bind("keyup.dialog", function (e) {
+            if (e.which == 27)
+                hideDialog(dialog);
+        });
+        content.click(function (e) {
+            e.stopPropagation();
+        });
+    }
+    setTimeout(function () {
+        dialog.css({opacity: 1});
+        if (options.onLoaded)
+            options.onLoaded();
+    }, 1);
+}
+
+function hideDialog(dialog) {
+    $(document).unbind("keyup.dialog");
+    dialog.css({opacity: 0});
+    setTimeout(function () {
+        dialog.remove();
+    }, 400);
+}
